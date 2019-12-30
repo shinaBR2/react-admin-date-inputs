3363801e682a025114ffae5c0b298ba07047d90e
asvarcas <asvarcas@etriek.com.uy>
2019-12-30 17:04:46 -0300

    Work on adapting to version 3 of react-admin.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {useInput, FieldTitle} from 'ra-core';
+import {useInput, useTranslate, FieldTitle} from 'ra-core';
+import {InputHelperText} from 'ra-ui-materialui';
+
 import {
     DatePicker,
     TimePicker,
@@ -10,71 +12,126 @@ import {
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
+const sanitizeRestProps = ({
+    allowEmpty,
+    alwaysOn,
+    basePath,
+    component,
+    defaultValue,
+    formClassName,
+    initializeForm,
+    input,
+    isRequired,
+    label,
+    limitChoicesToValue,
+    locale,
+    meta,
+    options,
+    optionText,
+    optionValue,
+    record,
+    resource,
+    source,
+    textAlign,
+    translate,
+    translateChoice,
+    labelTime,
+    ...rest
+}) => rest;
+
 const makePicker = PickerComponent => {
-    const _makePicker = props => {
+    const _makePicker = ({
+        format = 'yyyy/MM/dd',
+        label,
+        options,
+        source,
+        resource,
+        helperText,
+        margin = 'dense',
+        onBlur = () => null,
+        onChange = () => null,
+        onFocus,
+        parse,
+        validate,
+        variant = 'filled',
+        defaultValue,
+        providerOptions,
+        ...rest
+    }) => {
+        const translate = useTranslate();
+        const [selectedDate, handleDateChange] = useState(new Date());
         const {
             id,
             input,
             isRequired,
-            meta: {touched, error},
-        } = useInput(props);
-
-        const {
-            options,
-            label,
-            source,
+            meta: {error, touched},
+        } = useInput({
+            /* format, */
+            onBlur,
+            onChange,
+            onFocus,
+            parse,
             resource,
-            className,
-            margin,
-            variant,
-            providerOptions
-        } = props;
-
-        const onChange = (date, value) => {
-            input.onChange && input.onChange(date);
-            input.onBlur && input.onBlur();
-        };
-
+            source,
+            validate,
+            /* type: 'datetime-local', */
+            ...rest,
+        });
+        
         return (
-          <div className="picker">
-              <MuiPickersUtilsProvider {...providerOptions}>
-                  <PickerComponent
-                    id={id}
-                    {...input}
-                    label={<FieldTitle
-                      label={label}
-                      source={source}
-                      resource={resource}
-                      isRequired={isRequired}
-                    />}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    {...options}
-                    variant={variant}
-                    margin={margin}
-                    error={!!(touched && error)}
-                    helperText={touched && error}
-                    className={className}
-                    value={input.value ? input.value : null}
-                    onChange={onChange}
-                  />
-              </MuiPickersUtilsProvider>
-          </div>
+          <MuiPickersUtilsProvider {...providerOptions}>
+              <PickerComponent
+                id={id}
+                {...input}
+                label={<FieldTitle
+                  label={label}
+                  source={source}
+                  resource={resource}
+                  isRequired={isRequired}
+                />}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                {...options}
+                /* variant={"dialog" | "inline" | "static"} */
+                /* inputVariant={"standard" | "outlined" | "filled"} */
+                inputVariant={variant}
+                margin={margin}
+                error={!!(touched && error)}
+                helperText={
+                    (touched && error) || helperText ? (
+                      <InputHelperText
+                        touched={touched}
+                        error={error}
+                        helperText={helperText}
+                      />
+                    ) : null
+                }
+                value={selectedDate ? new Date(selectedDate) : null}
+                onBlur={() => onBlur(selectedDate ? new Date(selectedDate).toISOString() : null)}
+                onChange={handleDateChange}
+                onAccept={onChange}
+                clearLabel={translate('ra.action.clear_input_value')}
+                cancelLabel={translate('ra.action.cancel')}
+                /* okLabel={translate('ra.action.ok')} */
+                /* todayLabel={translate('ra.date.today')} */
+                {...sanitizeRestProps(rest)}
+              />
+          </MuiPickersUtilsProvider>
         );
     };
 
     _makePicker.propTypes = {
-        input: PropTypes.object,
         isRequired: PropTypes.bool,
         label: PropTypes.string,
-        onChange: PropTypes.func.isRequired,
-        value: PropTypes.any.isRequired,
+        onChange: PropTypes.func,
         meta: PropTypes.object,
         options: PropTypes.object,
         resource: PropTypes.string,
         source: PropTypes.string,
         labelTime: PropTypes.string,
+        margin: PropTypes.string,
+        variant: PropTypes.string,
         className: PropTypes.string,
         providerOptions: PropTypes.shape({
             utils: PropTypes.func,
@@ -83,7 +140,6 @@ const makePicker = PickerComponent => {
     };
 
     _makePicker.defaultProps = {
-        input: {},
         isRequired: false,
         label: '',
         meta: {touched: false, error: false},
