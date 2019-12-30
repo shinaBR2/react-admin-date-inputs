1422247790f469a4ee8c5244ef09aff5530d52d4
asvarcas <asvarcas@etriek.com.uy>
2019-12-30 12:46:51 -0300

    Convert to function component.

--- src/index.js
+++ src/index.js
@@ -11,96 +11,90 @@ import {
 import DateFnsUtils from '@date-io/date-fns';
 
 const makePicker = PickerComponent => {
-    class _makePicker extends Component {
-        onChange = (date) => {
-            this.props.input.onChange(date);
-            this.props.input.onBlur();
+    return props => {
+        const {
+            id,
+            input,
+            isRequired,
+            meta: {touched, error},
+        } = useInput(props);
+
+        const {
+            options,
+            label,
+            source,
+            resource,
+            className,
+            margin,
+            variant,
+            providerOptions
+        } = props;
+
+        const onChange = (date) => {
+            props.input.onChange(date);
+            props.input.onBlur();
         };
 
-        render() {
-            const {
-                options,
-                label,
-                source,
-                resource,
-                className,
-                margin,
-                variant,
-                providerOptions,
-            } = this.props;
-
-            const {
-                id,
-                input,
-                isRequired,
-                meta: { touched, error },
-            } = useInput(this.props);
-
-            return (
-              <div className="picker">
-                  <MuiPickersUtilsProvider {...providerOptions}>
-                      <PickerComponent
-                        id={id}
-                        {...input}
-                        label={<FieldTitle
-                          label={label}
-                          source={source}
-                          resource={resource}
-                          isRequired={isRequired}
-                        />}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        {...options}
-                        variant={variant}
-                        margin={margin}
-                        error={!!(touched && error)}
-                        helperText={touched && error}
-                        ref={node => {
-                            this.picker = node;
-                        }}
-                        className={className}
-                        value={input.value ? input.value : null}
-                        onChange={this.onChange}
-                      />
-                  </MuiPickersUtilsProvider>
-              </div>
-            );
-        }
+        return (
+          <div className="picker">
+              <MuiPickersUtilsProvider {...providerOptions}>
+                  <PickerComponent
+                    id={id}
+                    {...input}
+                    label={<FieldTitle
+                      label={label}
+                      source={source}
+                      resource={resource}
+                      isRequired={isRequired}
+                    />}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    {...options}
+                    variant={variant}
+                    margin={margin}
+                    error={!!(touched && error)}
+                    helperText={touched && error}
+                    className={className}
+                    value={input.value ? input.value : null}
+                    onChange={onChange}
+                  />
+              </MuiPickersUtilsProvider>
+          </div>
+        );
     }
+};
 
-    _makePicker.propTypes = {
-        input: PropTypes.object,
-        isRequired: PropTypes.bool,
-        label: PropTypes.string,
-        meta: PropTypes.object,
-        options: PropTypes.object,
-        resource: PropTypes.string,
-        source: PropTypes.string,
-        labelTime: PropTypes.string,
-        className: PropTypes.string,
-        providerOptions: PropTypes.shape({
-            utils: PropTypes.func,
-            locale: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-        }),
-    };
+makePicker.propTypes = {
+    input: PropTypes.object,
+    isRequired: PropTypes.bool,
+    label: PropTypes.string,
+    meta: PropTypes.object,
+    options: PropTypes.object,
+    resource: PropTypes.string,
+    source: PropTypes.string,
+    labelTime: PropTypes.string,
+    className: PropTypes.string,
+    providerOptions: PropTypes.shape({
+        utils: PropTypes.func,
+        locale: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    }),
+};
 
-    _makePicker.defaultProps = {
-        input: {},
-        isRequired: 'false',
-        label: '',
-        meta: {touched: false, error: false},
-        options: {},
-        resource: '',
-        source: '',
-        labelTime: '',
-        className: '',
-        providerOptions: {
-            utils: DateFnsUtils,
-            locale: undefined,
-        },
-    };
-    return _makePicker;
+makePicker.defaultProps = {
+    input: {},
+    isRequired: 'false',
+    label: '',
+    meta: {touched: false, error: false},
+    options: {},
+    resource: '',
+    source: '',
+    labelTime: '',
+    className: '',
+    providerOptions: {
+        utils: DateFnsUtils,
+        locale: undefined,
+    },
 };
 
 export const DateInput = makePicker(DatePicker);
