6a0df403fc9b79a63f5601ca3be23f1100a1bfe9
asvarcas <asvarcas@etriek.com.uy>
2020-01-07 11:54:21 -0300

    Build 3.0.18 - Converted files to UTF-8.

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@ const convertDateToString = (value) => {
     if (!(value instanceof Date) || isNaN(value.getDate())) {
         return '';
     }
-    const yyyy = leftPad4(value.getFullYear());
+    const yy = leftPad4(value.getFullYear());
     const MM = leftPad2(value.getMonth() + 1);
     const dd = leftPad2(value.getDate());
     const hh = leftPad2(value.getHours());
     const mm = leftPad2(value.getMinutes());
-    return `${yyyy}-${MM}-${dd}T${hh}:${mm}`;
+    return `${yy}-${MM}-${dd}T${hh}:${mm}`;
 };
 
 // yyyy-MM-ddThh:mm
