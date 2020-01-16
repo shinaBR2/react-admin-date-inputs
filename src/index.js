b17bcdc2603cbcd8dd7acf150810aa5ebc2415e4
asvarcas <asvarcas@etriek.com.uy>
2020-01-16 18:22:42 -0300

    Change variables location.

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ import {
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
-const leftPad = (nb = 2) => value => ('0'.repeat(nb) + value).slice(-nb);
-const leftPad4 = leftPad(4);
-const leftPad2 = leftPad(2);
-
 /**
  * @param {Date} value value to convert
  * @returns {String} A standardized datetime (yyyy-MM-ddThh:mm), to be passed to an <input type="datetime-local" />
@@ -26,6 +22,11 @@ const convertDateToString = (value) => {
     if (!(value instanceof Date) || isNaN(value.getDate())) {
         return '';
     }
+
+    const leftPad = (nb = 2) => value => ('0'.repeat(nb) + value).slice(-nb);
+    const leftPad4 = leftPad(4);
+    const leftPad2 = leftPad(2);
+
     const yy = leftPad4(value.getFullYear());
     const MM = leftPad2(value.getMonth() + 1);
     const dd = leftPad2(value.getDate());
@@ -33,9 +34,6 @@ const convertDateToString = (value) => {
     const mm = leftPad2(value.getMinutes());
     return `${yy}-${MM}-${dd}T${hh}:${mm}`;
 };
-
-// yyyy-MM-ddThh:mm
-const dateTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
 
 /**
  * Converts a date from the Redux store, with timezone, to a date string
@@ -53,6 +51,10 @@ const dateTimeFormatter = (value) => {
     if (value instanceof Date) {
         return convertDateToString(value);
     }
+
+    // yyyy-MM-ddThh:mm
+    const dateTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
+
     // valid dates should not be converted
     if (dateTimeRegex.test(value)) {
         return value;
